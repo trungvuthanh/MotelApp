091b9146db818c06d182b91dcc6a4ace99caf9b2
nguyenhungduy <hungduy2000@gmail.com>
2020-12-19 18:26:38 +0700

    Finish Chat

---
+++ static/script/chat.js
@@ -0,0 +1,8 @@
+// Load menu
+$('#menu').load('../static/page/menu-admin.html');
+
+// 
+$('#searchOwner').load('../static/page/searchOwner.html');
+
+// Load footer
+$('#footer').load('../static/page/footer.html');
