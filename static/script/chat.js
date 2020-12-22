516f62ed5836c117fdb61c2320aea29e7cae7602
nguyenhungduy <hungduy2000@gmail.com>
2020-12-22 15:41:32 +0700

    Finish chat

--- static/script/chat.js
+++ static/script/chat.js
@@ -13,12 +13,83 @@ $("#search-owner").blur(function() {
     $(".history-chat").css("display", "block"); 
 });
 
+function renderImageRead (typeRead, typeAvt) {
+    if (typeRead == "read") {
+        return "" 
+    } else if (typeRead =="read-recent") {
+        return '<img class="status-message-read-recent" src="' + typeAvt + '.png">'
+    } else if (typeRead == "unread") {
+        return '<img class="status-message" src="../static/image/unread.png">'
+    } else if (typeRead == "loading-message") {
+        return '<img class="status-message" src="../static/image/loading-message.png">'
+    }
+}
+
+
+function renderMessageToHTML(sender, text, typeRead, typeAvt) {
+    if (sender == "you") {
+        return '<div class="col-sm-12 mt-1">' +
+                    '<div class="row">' +
+                        '<div class="col-sm-1 pl-0 pr-0">' +
+                            '<img class="avatarOwner" src="' + typeAvt + '.png">' +
+                        '</div>' +
+                        '<div class="col-sm-8 mt-0 pl-0">' +                     
+                            '<div class="chatFromYou">'+ text +'</div>' + 
+                        '</div>' + 
+                        '<div class="col-sm-3 each-message">' + renderImageRead(typeRead, typeAvt) + '</div>' +
+                    '</div>' +
+                '</div>'
+    } else if (sender == "me") {
+        return '<div class="col-sm-12 mt-2">' +
+                    '<div class="row m-0">' +
+                        '<div class="col-sm-4 pl-0"></div>' +
+                        '<div class="col-sm-8 pl-2 pr-2 pt-1">' +
+                            '<span class="chatFromMe ">' + text + '</span>' +                          
+                            renderImageRead(typeRead, typeAvt) +               
+                        '</div>' + 
+                    '</div>' +
+                '</div>' 
+    }
+}
+
+function autoScroll() {
+    $('#message-right').animate({
+        scrollTop: $('#message-right').get(0).scrollHeight
+    }, 100);
+}
+autoScroll()
+
+function sendMessage() {
+    typeAvt = document.getElementById("avtOwner").src.replace (/[^\d.]/g,''); 
+    text = document.getElementById("input-message").value
+    document.getElementById("input-message").value = ""
+    sender = "me"
+    typeRead = "loading-message"
+    console.log(renderMessageToHTML(sender, text, typeRead, typeAvt) )
+    document.getElementById("message-right").innerHTML += renderMessageToHTML(sender, text, typeRead, typeAvt) 
+    autoScroll()
+    document.getElementById("input-message").focus()
+    // Sau này call API
+
+}
+
+document.getElementById("send-message").onclick = ()=> {
+    sendMessage()
+}
+
+$(document.getElementById("input-message")).keypress((e) => {
+    if (e.keyCode == 13) {
+        sendMessage()
+    }
+});
+document.getElementsByClassName("send-message").onclick = renderMessageToHTML()
+
 document.querySelectorAll(".each-message-history").forEach(element => {
     element.onclick = function() {
         document.querySelectorAll(".each-message-history").forEach(element => {
-            element.classList.remove("active")
+            element.classList.remove("active-chat")
         });
-        this.classList.add("active")
+        this.classList.add("active-chat")
     }   
 });
 
